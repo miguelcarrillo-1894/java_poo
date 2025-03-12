a9eb4a7a6126a8a1df279d91a9897fb05f648d80
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-11 20:09:11 -0500

    Update Principal.java

--- ControlAireAcondicionado/src/controlaireacondicionado/Principal.java
+++ ControlAireAcondicionado/src/controlaireacondicionado/Principal.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package controlaireacondicionado;
 
-/**
- *
- * @author Usuario
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        SistemaAire sistemaAire = new SistemaAire();
         sistemaAire.iniciar();
