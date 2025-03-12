5ae8e6a6f1fed00279da1711a2d0aa1c0790cbaf
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-11 20:01:19 -0500

    Update FizzBuzz.java

--- Fizzbuzzz/src/fizzbuzzz/FizzBuzz.java
+++ Fizzbuzzz/src/fizzbuzzz/FizzBuzz.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fizzbuzzz;
 
-/**
- *
- * @author Usuario
- */
 public class FizzBuzz {
 
     public void jugar() {
