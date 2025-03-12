6b8310cd74cd4e86c848e4c92cca51576338d45b
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-11 20:00:34 -0500

    Update Principal.java

--- Fizzbuzzz/src/fizzbuzzz/Principal.java
+++ Fizzbuzzz/src/fizzbuzzz/Principal.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package fizzbuzzz;
 
-/**
- *
- * @author Usuario
- */
 public class Principal {
      public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
