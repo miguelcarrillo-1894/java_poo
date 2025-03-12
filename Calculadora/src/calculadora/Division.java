e75762d26299960f49ccc0b337419a51e6605eca
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-11 20:14:10 -0500

    Update Division.java

--- Calculadora/src/calculadora/Division.java
+++ Calculadora/src/calculadora/Division.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package calculadora;
 
-/**
- *
- * @author Usuario
- */
 public class Division {
     public double calcular(double a, double b) {
         if (b == 0) {
