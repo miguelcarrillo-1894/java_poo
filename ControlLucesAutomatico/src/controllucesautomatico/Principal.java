6ae4c7c52a7e6467f4cbdbf6ed9400707740641d
miguelcarrillo-1894 <miguelcarrillo.1894@gmail.com>
2025-03-11 20:08:24 -0500

    Update Principal.java

--- ControlLucesAutomatico/src/controllucesautomatico/Principal.java
+++ ControlLucesAutomatico/src/controllucesautomatico/Principal.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package controllucesautomatico;
 
-/**
- *
- * @author Usuario
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         SistemaLuces sistemaLuces = new SistemaLuces();
         sistemaLuces.iniciar();
